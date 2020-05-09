009a365001107a4b4948f7a1caade7fbe713528d
daimori <anraku90@gmail.com>
2020-05-09 11:32:18 +0900

    create markdown editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import ReactMarkdown from 'react-markdown';
+/** @jsx jsx */
+import { jsx } from '@emotion/core'
 
 function App() {
+  const input = '# This is a header\n\nAnd this is a paragraph'
+  const [text, setText] = React.useState(input)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div css={{
+      display: "flex",
+      justifyContent: "space-between",
+    }}>
+      <div>
+        <textarea rows={40} cols={40} value={text} onChange={e => setText(e.target.value)}/>
+      </div>
+      <div css={{
+      }}>
+        <ReactMarkdown source={text}></ReactMarkdown>
+      </div>
     </div>
   );
 }
